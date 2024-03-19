a86ebbeeccaee2e763275e784c4542739ef4e0e6
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-03-20 05:20:09 +0700

    output - export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    output: 'export',
+  output: 'export',
+  reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    NEXT_PUBLIC_CLOUDINARY_PRESET_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_PRESET_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME,
   },
   images: {
     domains: ["res.cloudinary.com"],
