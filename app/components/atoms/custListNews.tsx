980c768d6d39e544b19cb03bca734d73f5ffddd9
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-03-24 06:27:40 +0700

    [ADD] width on custListNews Title

--- app/components/atoms/custListNews.tsx
+++ app/components/atoms/custListNews.tsx
@@ -30,8 +30,8 @@ const CustListNews = ({ data }: INews) => {
   return (
     <div className="bg-slate-100 border p-4 flex flex-col justify-between">
       <div className="flex justify-between">
-        <h2 className="text-base md:text-lg font-semibold">{data.title}</h2>
-        <p className="text-xs hidden md:flex italic">{formatDate(data.date)}</p>
+        <h2 className="text-base md:text-lg font-semibold w-9/12">{data.title}</h2>
+        <p className="text-xs hidden md:flex italic whitespace-nowrap">{formatDate(data.date)}</p>
       </div>
       <div className="py-1">
         <p className="text-xs md:text-sm font-normal text-gray-500">
@@ -43,7 +43,7 @@ const CustListNews = ({ data }: INews) => {
       <div className="flex flex-col md:flex-row gap-2 md:gap-0 md:justify-between md:items-end">
         <span className="text-[10px]">{data.note}</span>
         <div className="flex justify-between items-end md:justify-end w-full md:w-fit">
-          <p className="text-[10px] flex md:hidden italic">
+          <p className="text-[10px] flex md:hidden italic whitespace-nowrap">
             {formatDate(data.date)}
           </p>
           <Link
--- app/components/section/news.tsx
+++ app/components/section/news.tsx
@@ -1,52 +1,40 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react";
-import CustCardArticle from "../atoms/custCardArticle";
 import CustTagTittle from "../atoms/custTagTittle";
-import prisma from "@/app/libs/prisma";
-import Link from "next/link";
+import React, { useState, useEffect } from 'react';
 import activitySlider from "@/app/helpers/activitySlider";
 import ActivitySlider from "../molecules/ActivitySlider";
 
 interface DataImage {
-  id: number,
-  id_listActivity: number,
-  url: string,
-  name: string,
-  description: string,
-  activity: string
+  id: number;
+  id_listActivity: number;
+  url: string;
+  name: string;
+  description: string;
+  activity: string;
 }
 
 const News = () => {
-
   const [dataSlider, setDataSlider] = useState<DataImage[]>([]);
 
   useEffect(() => {
-
-    activitySlider().then((res) => {
-      if (res.error === false) {
-        setDataSlider(res.data || []);
-      } else {
-        console.log(res.message);
-      }
-    }).catch((err) => {
-      console.log(err);
-    })
+    activitySlider()
+      .then((res) => {
+        if (res.error === false) {
+          setDataSlider(res.data || []);
+        } else {
+          console.log(res.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
     <div className="cust-container relative">
       <CustTagTittle text="Activity" />
-        <ActivitySlider dataSlider={dataSlider} />
-      {/* <div className="grid md:grid-cols-3 gap-5 pt-10 pb-5 w-full">
-        {
-            dataSlice.map((item, index) => {
-                return (
-                    <CustCardArticle key={index} data={item} />
-                )
-            })
-        }
-      </div> */}
+      <ActivitySlider dataSlider={dataSlider} />
       {/* <div className="flex justify-center mb-5">
         <Link
           href={"/news"}
