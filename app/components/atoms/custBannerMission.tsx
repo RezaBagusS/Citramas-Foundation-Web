c6b6e63c88a7c49a954ee5171832588d98e6145b
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-05-27 10:23:40 +0700

    [ADD] title on bannerMission

--- app/components/atoms/custBannerMission.tsx
+++ app/components/atoms/custBannerMission.tsx
@@ -1,12 +1,14 @@
 interface CustBannerMissionProps {
-    text: string;
+  desc: string;
+  title: string;
 }
 
-const CustBannerMission = ({ text }: CustBannerMissionProps) => {
+const CustBannerMission = ({ desc, title }: CustBannerMissionProps) => {
   return (
-    <div className="bg-slate-100 rounded-md px-5 py-4 ring-2 ring-custPrimary/30">
+    <div className="relative bg-slate-100 rounded-md px-5 py-4 ring-2 ring-custPrimary/30">
+      <h1 className="text-sm md:text-base italic font-bold absolute -top-5 md:-top-6 right-1/2 translate-x-1/2 ring-2 ring-custPrimary/30 bg-custPrimary text-custWhite -skew-y-12 rotate-12 px-3 py-1">{title}</h1>
       <p className="text-gray-600 text-xs md:text-sm text-start">
-        {text}
+        {desc}
       </p>
     </div>
   );
--- app/components/molecules/bannerMission.tsx
+++ app/components/molecules/bannerMission.tsx
@@ -1,19 +1,22 @@
 import CustBannerMission from "../atoms/custBannerMission";
 
 const dataBannerMission = [
-  "Organizing social welfare programs in the education sector; health; social; and sports development.",
-  "Create sustainable programs to improve the standard of living environment and create jobs that encourage individuals to improve their skills.",
+  {
+    title: "Our Mission",
+    desc: "Organizing social welfare programs in the education sector; health; social; and sports development."
+  },
+  {
+    title: "Our Vission",
+    desc: "Create sustainable programs to improve the standard of living environment and create jobs that encourage individuals to improve their skills."
+  },
 ];
 
 const BannerMission = () => {
   return (
-    <div className="relative w-full">
-      <h1 className="text-sm md:text-base italic font-bold absolute top-1 md:-top-1 right-1/2 translate-x-1/2 ring-2 ring-custPrimary/30 bg-custPrimary text-custWhite -skew-y-12 rotate-12 px-3 py-1">Our Vison</h1>
-      <div className="grid md:grid-cols-2 gap-5 px-5 pt-5 pb-10">
-        {dataBannerMission.map((item, index) => {
-          return <CustBannerMission key={index} text={item} />;
-        })}
-      </div>
+    <div className="grid w-full md:grid-cols-2 gap-8 md:gap-5 px-5 pt-5 pb-10">
+      {dataBannerMission.map((item, index) => {
+        return <CustBannerMission key={index} title={item.title} desc={item.desc} />;
+      })}
     </div>
   );
 };
